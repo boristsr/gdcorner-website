7c592eafcca407efef7a0c53636571bec66e59b5
Philip Edwards <philip@gdcorner.com>
2019-12-09 18:18:57 +1100

    Fix syntax error in build script

--- build/jenkins-docker-build.sh
+++ build/jenkins-docker-build.sh
@@ -3,7 +3,7 @@
 # Install modules for site
 bundle install
 
-BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:development}"
+BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-development}"
 
 # Build site
 JEKYLL_ENV=$BUILD_ENVIRONMENT bundle exec jekyll build
