0dd407b6b2e1dc890ab3af1b5c9a5877205a70ef
Philip Edwards <philip@gdcorner.com>
2019-12-09 16:25:02 +1100

    Update jenkins-docker-build.sh

--- build/jenkins-docker-build.sh
+++ build/jenkins-docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install some prereqs
-sudo apt-get install zlib1g zlib1g-dev
+sudo apt-get install -y zlib1g zlib1g-dev
 
 # Install ruby & bundler
-sudo apt-get install ruby ruby-dev
+sudo apt-get install -y ruby ruby-dev
 sudo gem install bundler
 
 # Install modules for site
