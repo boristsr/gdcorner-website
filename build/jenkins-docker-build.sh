2ca8d282fe75913ac7dba2aaeee932861a972aea
Philip Edwards <philip@gdcorner.com>
2019-12-17 14:55:46 +1100

    Enable URL override option

--- build/jenkins-docker-build.sh
+++ build/jenkins-docker-build.sh
@@ -5,6 +5,13 @@ bundle install
 
 BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-development}"
 
+if [[ -z "${OVERRIDE_URL}" ]]; then
+  sed -i _config.yml \
+        -e 's/url:.*/url: $OVERRIDE_URL/'
+
+  cat _config.yml
+fi
+
 echo Building in mode: $BUILD_ENVIRONMENT
 
 # Build site
