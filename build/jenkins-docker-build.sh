f5983bd4416ffd7665fb0432a82f3592667a4f8b
Philip Edwards <philip@gdcorner.com>
2022-06-13 11:35:49 +1000

    improved logging of build configuration setup

--- build/jenkins-docker-build.sh
+++ build/jenkins-docker-build.sh
@@ -5,15 +5,16 @@ BUILD_ENVIRONMENT="${BUILD_ENVIRONMENT:-development}"
 CONFIG_FILES="_config.yml"
 
 if [[ "${BRANCH_NAME}"=="uattest" ]]; then
-  echo "Development build, building with extra config data"
+  echo "Building with UAT Configuration"
   CONFIG_FILES="_config.yml,_config_uattest.yml"
   BUILD_ENVIRONMENT='staging'
 elif [[ "${BRANCH_NAME}"=="staging" ]]; then
+  echo "Building with STAGING Configuration"
   echo "Development build, building with extra config data"
   CONFIG_FILES="_config.yml,_config_staging.yml"
   BUILD_ENVIRONMENT='staging'
 elif [[ "${BRANCH_NAME}"=="production" ]]; then
-  echo "Development build, building with extra config data"
+  echo "Building with PRODUCTION Configuration"
   CONFIG_FILES="_config.yml,_config_production.yml"
   BUILD_ENVIRONMENT='production'
 fi
